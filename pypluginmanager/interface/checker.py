ed5c6ea2ce970ad251ba3609e66eb714bfdafb03
mzahr <amirreza.zahraei1@rbe.sk.ca>
2023-09-14 19:57:37 -0600

    the interface is well tested

--- pypluginmanager/interface/checker.py
+++ pypluginmanager/interface/checker.py
@@ -7,10 +7,12 @@ from pypluginmanager.interface.types import InterfaceTypes
 
 def interTypeW(path_config: str, path_repo: str, interface_type: InterfaceTypes):
 
-    with open(path_config, mode='r+', newline='') as config_file:
+    with open(path_config, mode='r') as config_file:
         data = json.load(config_file)
         data["interfaces"].update({path_repo: interface_type.name})
-        json.dump(config_file, data)
+
+    with open(path_config, mode='w') as file:
+        json.dump(data, file, indent=4)
 
 
 def checkVersion(path_config: str):
@@ -21,11 +23,12 @@ def checkVersion(path_config: str):
 
 
 def checkExistence(path_config: str):
-    with open(path_config, mode='r+', newline='') as config_file:
+    with open(path_config, mode='r') as config_file:
         data = json.load(config_file)
 
         for interface_path in data["interfaces"]:
             if not os.path.exists(interface_path):
                 data.pop(interface_path)
 
-        json.dump(config_file, data)
+    with open(path_config, mode='w') as file:
+        json.dump(data, file, indent=4)
--- pypluginmanager/interface/inter_core.py
+++ pypluginmanager/interface/inter_core.py
@@ -29,6 +29,3 @@ class CoreInterface:
         checker.checkExistence(config_path)
 
 
-
-
-
