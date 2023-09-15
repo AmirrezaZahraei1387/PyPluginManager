afc2fbb8b81b42fa3e307a8cb32d1bb4156ae6d7
mzahr <amirreza.zahraei1@rbe.sk.ca>
2023-09-14 20:10:57 -0600

    the way the plugins will be saved in the repo is created.

--- pypluginmanager/samplefiles/interface.py
+++ pypluginmanager/samplefiles/interface.py
@@ -1,1 +1,23 @@
 INTERFACE = {"path": "type"}
+
+
+PLUGIN_SAMPLE = {
+
+    "name": "",
+    "version": "",
+    "auther": "",
+    "auther-email": "",
+    "url": "",
+    "error-report-email": "",
+
+    "description": "",
+    "keywords": "",
+
+    "dependencies": {},
+    "python-version": "",
+    "repo-path": ""
+}
+
+DEPENDENCIES = {"name": ["status", "message"]}
+
+
