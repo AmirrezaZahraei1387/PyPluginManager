7ba955b9dd7788f292e33700d4fb9f6a300255de
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-09-16 10:44:28 -0600

    the __init__.py of sample files completed and the functionality for satisying req is completed

--- pypluginmanager/installerp/satdep.py
+++ pypluginmanager/installerp/satdep.py
@@ -4,8 +4,10 @@ for install.
 """
 import sys
 import subprocess
-from Vmanager import PyVersion
 import re
+import warnings
+
+from Vmanager import PyVersion
 
 
 INSTALL_STATE = [sys.executable, '-m', 'pip', 'install', 'name']
@@ -15,7 +17,17 @@ python_current_version = PyVersion(sys.version_info[0], sys.version_info[1], sys
 def install_package(package_name: str):
     INSTALL_STATE[4] = package_name
     result = subprocess.run(INSTALL_STATE, stdout=subprocess.PIPE)
-    return result.stdout, result.returncode
+    return  {package_name:[result.returncode, result.stdout]}
+
+
+def satisfy_dep(package_names: list):
+    dependencies = {}
+    for package in package_names:
+        result = install_package(package)
+        if result[package][0] != 0:
+            warnings.warn(result[package][0])
+        dependencies.update(install_package(package))
+    return dependencies
 
 
 def check_python_version(version_expr: str):
--- pypluginmanager/samplefiles/__init__.py
+++ pypluginmanager/samplefiles/__init__.py
@@ -1,1 +1,4 @@
 from pypluginmanager.samplefiles.interface import INTERFACE
+from pypluginmanager.samplefiles.interface import PLUGIN_SAMPLE
+from pypluginmanager.samplefiles.interface import DEPENDENCIES
+
