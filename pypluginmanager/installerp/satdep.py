ebf29594eb11d512283c076d2ad752a318742a66
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-09-15 22:41:50 -0600

    the function for checking python version is created

--- pypluginmanager/installerp/satdep.py
+++ pypluginmanager/installerp/satdep.py
@@ -4,19 +4,35 @@ for install.
 """
 import sys
 import subprocess
-import Vmanager
-
+from Vmanager import PyVersion
+import re
 
 INSTALL_STATE = [sys.executable, '-m', 'pip', 'install', 'name']
+python_current_version = PyVersion(sys.version_info[0], sys.version_info[1], sys.version_info[2])
 
 
 def install_package(package_name: str):
-
     INSTALL_STATE[4] = package_name
     result = subprocess.run(INSTALL_STATE, stdout=subprocess.PIPE)
     return result.stdout, result.returncode
 
 
-def check_python_version(version: Vmanager.PyVersion):
+def check_python_version(version_expr: str):
+    tokens = re.search(r"\s*([\d.]+)", version_expr)
+    try:
+        # it means we have passed a version_expr with a python version
+        # domain or exact
+        v = tokens.group()
 
-    python_current_version = Vmanager.PyVersion(sys.version_info[0], sys.version_info[1], sys.version_info[2])
+        v_expected = PyVersion().fromstr(v)
+
+        version_expr = version_expr.replace(v, v_expected.__repr__(), 1)
+
+        version_expr = python_current_version.__repr__() + version_expr
+        return eval(version_expr)
+
+    except AttributeError:
+        return True
+
+
+
