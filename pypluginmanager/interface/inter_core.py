5ee211c8658609f5908ae7f9cb9728baa12fd618
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-09-23 17:29:10 -0600

    the operation for unistalling a plugin is created

--- pypluginmanager/interface/inter_core.py
+++ pypluginmanager/interface/inter_core.py
@@ -8,10 +8,10 @@ import pypluginmanager
 from pypluginmanager.interface.types import InterfaceTypes
 from pypluginmanager.interface import checker
 import pypluginmanager.installerp as installerp
+import pypluginmanager.exceptions as exps
 
 
 class CoreInterface:
-
     __type: InterfaceTypes
 
     def __init__(self, path_repo: str, interface_type: InterfaceTypes):
@@ -31,17 +31,22 @@ class CoreInterface:
                           v + " but is being opened with version " + pypluginmanager.__version__)
         checker.checkExistence(config_path)
 
-    def install(self, plugin_path):
+    def install(self, path):
 
-        with open(os.path.join(plugin_path, "plugin_config.json"), mode='r') as file:
+        with open(os.path.join(path, "plugin_config.json"), mode='r') as file:
             data = json.load(file)
             checker.check_existance(data, checker.MANDATORY_PARA)
+
+            if self.plugin_exist(data["name"]):
+                raise exps.PluginFoundError("the plugin requested to be installed already"
+                                            "exist")
 
             installerp.check_python_version(data["python-version"])
             result_dep_install = installerp.satisfy_dep(data["dependencies"])
             data["dependencies"] = result_dep_install
             full_installation_path = os.path.join(self.__plugins_dir, data["repo-path"])
-            shutil.copytree(plugin_path, full_installation_path)
+
+            shutil.copytree(path, full_installation_path)
 
             with open(self.__plugins_db, mode='r') as file_r:
                 d = json.load(file_r)
@@ -51,13 +56,24 @@ class CoreInterface:
                 with open(self.__plugins_db, mode='w') as file_w:
                     json.dump(d, file_w, indent=6)
 
-    def name_exist(self, plugin_name: str):
+    def plugin_exist(self, name: str):
         with open(self.__plugins_db, mode='r') as file_r:
             plugins = json.load(file_r)
-            return plugin_name in plugins
+            for name_ in plugins:
+                if name == name_:
+                    return True
+        return False
 
-    def uninstall(self, plugin_name):
+    def uninstall(self, name: str):
+        with open(self.__plugins_db, mode='r') as file_r:
+            plugins = json.load(file_r)
 
+            if not self.plugin_exist(name):
+                raise exps.PluginNotFoundError("the plugin specified to be deleted does not exist.")
+
+            path = os.path.join(self.__plugins_dir, plugins[name]["repo-path"])
+            del plugins[name]
+            shutil.rmtree(path)
 
 
 
